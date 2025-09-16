811fd513e1f14451556d166d4aaff4d198b7e405
Heera Singh <heera.madquick@gmail.com>
2025-09-16 21:41:28 +0530

    change branding

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -16,7 +16,7 @@ import { getServerSideURL } from '@/utilities/getURL'
 import { s3Storage } from '@payloadcms/storage-s3'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title ? `${doc.title} | Fermi.it` : 'Fermi.it'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -9,8 +9,8 @@ const defaultOpenGraph: Metadata['openGraph'] = {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Fermi.it',
+  title: 'Fermi.it',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
